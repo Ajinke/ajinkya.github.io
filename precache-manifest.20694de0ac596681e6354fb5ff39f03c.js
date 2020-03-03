8448895c8910160bde7c65b34a16198c868cc1f1
Ajinke <59605365+Ajinke@users.noreply.github.com>
2020-03-03 15:07:05 +0530

    Add files via upload

---
+++ precache-manifest.20694de0ac596681e6354fb5ff39f03c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89c9c251897b07275ec3806d68daf70e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "700a378d66a7d1021108",
+    "url": "/static/css/2.00269225.chunk.css"
+  },
+  {
+    "revision": "eacb0fb0e40e7a286858",
+    "url": "/static/css/main.f9548bac.chunk.css"
+  },
+  {
+    "revision": "700a378d66a7d1021108",
+    "url": "/static/js/2.29a785cd.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/static/js/2.29a785cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eacb0fb0e40e7a286858",
+    "url": "/static/js/main.56700b93.chunk.js"
+  },
+  {
+    "revision": "833bb630cf8d9608576f",
+    "url": "/static/js/runtime-main.617b59c0.js"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ed0943f224d6a39871066f569a18fa60",
+    "url": "/static/media/weathericons-regular-webfont.ed0943f2.svg"
+  }
+]);
